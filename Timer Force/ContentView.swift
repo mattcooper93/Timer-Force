222318446eb79e543b793978529a5e73b73eb3e0
mattcooper93 <matt@mattcooper.com>
2025-11-17 19:02:10 -0500

    Initial Commit

---
+++ Timer Force/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timer Force
+//
+//  Created by Matt Cooper on 11/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timer Force/Timer_ForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_ForceApp.swift
+//  Timer Force
+//
+//  Created by Matt Cooper on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Timer_ForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
